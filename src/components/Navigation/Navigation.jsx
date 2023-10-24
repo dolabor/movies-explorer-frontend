966819ad096ff55f13f22c20feec44f954804c67
Evgeniia Chigzhit <jeniachigzhit@gmail.com>
2023-10-25 01:06:44 +0200

    feat: Overlay in Navigation is ready

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -11,42 +11,39 @@ function Navigation({isOpen, onClose}) {
 
   return (
     isOpen ? (
-      <section className="navigation">
-        <button className="navigation__close-button button" onClick={onClose}>
-          <img src={closeMenuButton} alt="Кнопка закрытия меню"/>
-        </button>
-        <div className="navigation__content">
-          <div className="navigation__movies-area">
-            <NavLink to="/movies" className="navigation__link">
-              Главная
-            </NavLink>
-            <NavLink to="/movies" className="navigation__link">
-              Фильмы
-            </NavLink>
-            <NavLink to="/saved-movies" className="navigation__link">
-              Сохранённые фильмы
-            </NavLink>
+      <>
+        <div className="navigation__overlay" onClick={onClose}></div>
+        <section className="navigation">
+          <button className="navigation__close-button button" onClick={onClose}>
+            <img className="navigation__close-icon" src={closeMenuButton} alt="Кнопка закрытия меню"/>
+          </button>
+          <div className="navigation__content">
+            <div className="navigation__links">
+              <NavLink
+                to="/"
+                className={`navigation__link ${isActive('/') ? 'navigation__link_active' : ''}`}>
+                Главная
+              </NavLink>
+              <NavLink
+                to="/movies"
+                className={`navigation__link ${isActive('/movies') ? 'navigation__link_active' : ''}`}>
+                Фильмы
+              </NavLink>
+              <NavLink
+                to="/saved-movies"
+                className={`navigation__link ${isActive('/saved-movies') ? 'navigation__link_active' : ''}`}>
+                Сохранённые фильмы
+              </NavLink>
+            </div>
+            <div className="navigation__profile">
+              <NavLink to="/profile" className="navigation__profile-link">
+                Аккаунт
+              </NavLink>
+              <div className="navigation__profile-icon"></div>
+            </div>
           </div>
-          <div className="navigation__profile">
-            <NavLink
-              to="/"
-              className={`navigation__profile-link ${isActive('/') ? 'navigation__profile-link_active' : ''}`}>
-              Главная
-            </NavLink>
-            <NavLink
-              to="/movies"
-              className={`navigation__profile-link ${isActive('/movies') ? 'navigation__profile-link_active' : ''}`}>
-              Фильмы
-            </NavLink>
-            <NavLink
-              to="/saved-movies"
-              className={`navigation__profile-link ${isActive('/saved-movies') ? 'navigation__profile-link_active' : ''}`}>
-              Сохранённые фильмы
-            </NavLink>
-            <div className="navigation__profile-icon"></div>
-          </div>
-        </div>
-      </section>
+        </section>
+      </>
     ) : ""
   );
 }
