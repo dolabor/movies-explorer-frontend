1164551231a39182bbd48fef2042b6ff97ee4290
Evgeniia Chigzhit <jeniachigzhit@gmail.com>
2023-11-11 20:02:17 +0100

    fix: Movies search changed

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -18,6 +18,7 @@ function Movies({isLoading, onCardLike, likedMovies}) {
     const storedSearchQuery = localStorage.getItem('searchQuery') || '';
     const storedIsShortMovie = localStorage.getItem('isShortMovie') === 'true';
     const storedFoundMovies = JSON.parse(localStorage.getItem('foundMovies')) || [];
+    console.log(storedFoundMovies);
 
     setSearchQuery(storedSearchQuery);
     setIsShortMovie(storedIsShortMovie);
@@ -26,6 +27,7 @@ function Movies({isLoading, onCardLike, likedMovies}) {
       setFoundMovies(storedFoundMovies);
     }
   }, []);
+
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
