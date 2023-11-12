de93ed20fbd69e5d05fba54158b7b2c51fca21d9
Evgeniia Chigzhit <jeniachigzhit@gmail.com>
2023-11-12 21:24:43 +0100

    fix: ProtectedRoute.jsx updated

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Navigate } from "react-router-dom";
 
 const ProtectedRouteElement = ({ element: Component, isLoggedIn  }) => {
   return (
-    isLoggedIn ? Component : <Navigate to="/" replace/>
+    useMemo(() => isLoggedIn ? Component : <Navigate to="/" replace/>, [isLoggedIn, Component])
 )}
 
 export default ProtectedRouteElement;
