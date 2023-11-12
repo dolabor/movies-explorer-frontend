a19243ae0a80fad243ea060612c657b4a04ef9e8
Evgeniia Chigzhit <jeniachigzhit@gmail.com>
2023-11-12 20:42:39 +0100

    fix: error state changed

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -4,13 +4,12 @@ import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import Preloader from '../Preloader/Preloader';
 import {moviesApi} from '../../utils/MoviesApi';
 
-function Movies({ isLoading, onCardLike, likedMovies }) {
+function Movies({isLoading, onCardLike, likedMovies}) {
   const storedSearchQuery = localStorage.getItem('searchQuery') || '';
   const storedIsShortMovie = localStorage.getItem('isShortMovie') === 'true';
   const [searchQuery, setSearchQuery] = useState(storedSearchQuery);
   const [foundMovies, setFoundMovies] = useState([]);
-  const [error, setError] = useState('');
-  const [searchedOnce, setSearchedOnce] = useState(false);
+  const [error, setError] = useState('Начните поиск');
   const [isCardListVisible, setIsCardListVisible] = useState(true);
   const [isShortMovie, setIsShortMovie] = useState(storedIsShortMovie);
 
@@ -28,7 +27,10 @@ function Movies({ isLoading, onCardLike, likedMovies }) {
             movie.nameRU.toLowerCase().includes(searchQuery.toLowerCase())
         );
         setFoundMovies(filteredMovies);
-        setSearchedOnce(true);
+
+        if (filteredMovies.length === 0) {
+          setError("Ничего не найдено");
+        }
       })
       .catch(() => {
         setError('Во время запроса произошла ошибка. Возможно, проблема с соединением или сервер недоступен. Подождите немного и попробуйте ещё раз');
@@ -46,14 +48,13 @@ function Movies({ isLoading, onCardLike, likedMovies }) {
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
-    setSearchedOnce(true);
-    updateCardList();
 
     if (searchQuery.trim() === '') {
       setError('Нужно ввести ключевое слово');
       setIsCardListVisible(false);
     } else {
       setError('');
+      updateCardList();
       setIsCardListVisible(true);
       localStorage.setItem('searchQuery', searchQuery);
     }
@@ -74,24 +75,17 @@ function Movies({ isLoading, onCardLike, likedMovies }) {
         isShortMovie={isShortMovie}
         handleShortMoviesToggle={handleShortMoviesToggle}
       />
-      {isLoading ? (
-        <Preloader/>
-      ) : searchedOnce ? (
-        foundMovies.length > 0 ? (
-          <MoviesCardList
-            data={foundMovies}
-            likedMovies={likedMovies}
-            onCardLike={onCardLike}
-            isCardListVisible={isCardListVisible}
-            isShortMovie={isShortMovie}
-            isShowMoreEnabled={true}
-          />
-        ) : (
-          <p className="movies__status">{error || 'Ничего не найдено'}</p>
-        )
-      ) : (
-        <p className="movies__status">Начните поиск</p>
-      )}
+      {isLoading && <Preloader/>}
+      {error && <p className="movies__status">{error}</p>}
+      {searchQuery !== "" && !error && foundMovies.length > 0 &&
+        <MoviesCardList
+          data={foundMovies}
+          likedMovies={likedMovies}
+          onCardLike={onCardLike}
+          isCardListVisible={isCardListVisible}
+          isShortMovie={isShortMovie}
+          isShowMoreEnabled={true}
+        />}
     </main>
   );
 }
