5a15fe20bee4a663c20f33c40b64e311f0783092
Evgeniia Chigzhit <jeniachigzhit@gmail.com>
2023-11-13 01:48:52 +0100

    fix: SavedMovies.jsx card filter updated

--- src/components/SavedMovies/SavedMovies.jsx
+++ src/components/SavedMovies/SavedMovies.jsx
@@ -1,31 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import SearchForm from '../SearchForm/SearchForm';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import Preloader from '../Preloader/Preloader';
-import { shortMoviesDuration } from '../../utils/constants';
 
-function SavedMovies({ data, isLoading, handleLikeClick }) {
+function SavedMovies({data, isLoading, handleLikeClick}) {
   const [searchQuery, setSearchQuery] = useState('');
   const [foundMovies, setFoundMovies] = useState([]);
   const [error, setError] = useState('');
   const [isShortMovie, setIsShortMovie] = useState(false);
-
-  const updateCardList = (shortMoviesState = isShortMovie) => {
-    setError('');
-
-    const filteredMovies = data.filter(
-      (movie) =>
-        movie.nameRU.toLowerCase().includes(searchQuery.toLowerCase()) &&
-        (shortMoviesState ? movie.duration <= shortMoviesDuration : true)
-    );
-    setFoundMovies(filteredMovies);
-
-    if (filteredMovies.length === 0) {
-      setError('Ничего не найдено');
-    } else {
-      setError('');
-    }
-  };
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
@@ -35,22 +17,23 @@ function SavedMovies({ data, isLoading, handleLikeClick }) {
       setFoundMovies([]);
     } else {
       setError('');
-      updateCardList();
-    }
-  };
 
-  const handleShortMoviesToggle = () => {
-    setIsShortMovie(!isShortMovie);
-    updateCardList(!isShortMovie);
+      const filteredMovies = data.filter(
+        (movie) =>
+          movie.nameRU.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+      setFoundMovies(filteredMovies);
+    }
   };
 
   const handleSearchChange = (e) => {
     setSearchQuery(e.target.value);
+    setError('');
   };
 
   useEffect(() => {
-    updateCardList();
-  }, []);
+    setFoundMovies([]);
+  }, [data]);
 
   return (
     <main className="saved-movies">
@@ -60,20 +43,20 @@ function SavedMovies({ data, isLoading, handleLikeClick }) {
         error={error}
         handleSearchChange={handleSearchChange}
         setIsShortMovie={setIsShortMovie}
-        handleSearchSubmit={handleSearchSubmit}
-        handleShortMoviesToggle={handleShortMoviesToggle}
+        onSearchSubmit={handleSearchSubmit}
       />
-      {isLoading && <Preloader/>}
-      {!isLoading && foundMovies.length > 0 &&
+      {isLoading ? (
+        <Preloader/>
+      ) : (
         <MoviesCardList
-          data={foundMovies}
+          data={foundMovies.length > 0 ? foundMovies : data}
           likedMovies={data}
           isShortMovie={isShortMovie}
-          onCardLike={(card) => handleLikeClick(card, true)}
+          onCardLike={card => handleLikeClick(card, true)}
           isShowMoreEnabled={false}
           isCardListVisible={true}
         />
-      }
+      )}
       <div className="saved-movies__divider"></div>
     </main>
   );
